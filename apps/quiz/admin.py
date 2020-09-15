75113c7aaf7693df4d31d1941e2051a65e604b9c
svalenzuela <StephanieEmelina13@gmail.com>
2020-09-15 12:34:03 -0400

    deleted answers model

--- apps/quiz/admin.py
+++ apps/quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.quiz.models import Quiz, Question
+
+
 
 # Register your models here.
+admin.site.register(Quiz)
+admin.site.register(Question)
--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -1,37 +1,43 @@
 from django.db import models
 from apps.authentication.models import User
+#from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 class Quiz(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
+    directions = models.CharField(max_length=250)
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.question
 
 
 class Question(models.Model):
     class Meta:
         verbose_name_plural = 'questions'
 
-    quiz = models.ForeignKey(Quiz, related_name='questions')
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
     question = models.CharField(max_length=100)
-    #description = models.CharField(max_length=100) <- Should I involve this even though I have questions above
     order = models.IntegerField(default=0)
-    choices = models.CharField(max_length=100)
+    choice = models.IntegerField(default=4)
     answer = models.CharField(max_length=100)
 
-class Answer(models.Model):
-    class Meta:
-        verbose_name_plural = 'answers'
-
-    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
-    description = models.CharField(max_length=100)
-    is_correct = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.is_correct
+        return self.answer
+
+
+# class Answer(models.Model):
+#     class Meta:
+#         verbose_name_plural = 'answers'
+#
+#     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+#     answer = models.CharField(max_length=100)
+#     is_correct = models.BooleanField(default=False)
+#
+#
+#     def __str__(self):
+#         return self.answer
