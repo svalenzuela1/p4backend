4058a4de44838d3eb4ae6daef59d41bc0c8bc33b
svalenzuela <StephanieEmelina13@gmail.com>
2020-09-15 21:53:29 -0400

    Quiz viewset

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -10,7 +10,36 @@ from apps.quiz.serializers import QuizSerializer, QuestionSerializer
 
 
 # Create your views here.
+class QuizViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = QuizSerializer
 
+    def get_queryset(self):
+        queryset = Quiz.objects.all().filter(owner=self.request.user)
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        quiz = Quiz.objects.filter(
+            name=request.data.get('name'),
+            owner=request.user
+        )
+
+        if quiz:
+            msg = 'Quiz with that name already exists'
+            raise ValidationError(msg)
+        return super().create(request)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        quiz = Quiz.objects.get(pk=self.kwargs["pk"])
+        if not request.user == quiz.owner:
+            raise PermissionDenied("You cannot delete this quiz")
+
+        return Response({
+            super().destroy(request, *args, **kwargs)
+        })
 
 
 
